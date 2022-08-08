91e156014bbd27b0162e7fc99ed0f4b39d331e55
Pratik <93422248+PratikHGhadge@users.noreply.github.com>
2022-08-08 22:48:19 +0530

    Time: 6 ms (5.74%), Space: 42.3 MB (51.88%) - LeetHub

--- 58-length-of-last-word/58-length-of-last-word.java
+++ 58-length-of-last-word/58-length-of-last-word.java
@@ -1,6 +1,6 @@
 class Solution {
     public int lengthOfLastWord(String s) {
-        String[] words = s.trim().split(" ");
+        String[] words = s.split("[ ]+");
 		return words[words.length -1].length();
     }
 }
