02bf0c9558a43be50972f768cd4fbdf82502d535
Pratik <93422248+PratikHGhadge@users.noreply.github.com>
2022-08-18 21:06:16 +0530

    Time: 313 ms (5.02%), Space: 85.3 MB (62.06%) - LeetHub

--- 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.java
+++ 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.java
@@ -4,11 +4,7 @@ class Solution {
         for(int i = 0; i<arr.length; i++){
             map.put(arr[i], map.getOrDefault(arr[i], 0)+1);
         }
-        Iterator <Integer> it = map.values().iterator(); 
-        ArrayList<Integer> list = new ArrayList<>();
-        while(it.hasNext()){
-            list.add(it.next());
-        }
+        ArrayList<Integer> list = new ArrayList<>(map.values());
         Collections.sort(list, Collections.reverseOrder());
         int count = 0;
         int n = arr.length;
