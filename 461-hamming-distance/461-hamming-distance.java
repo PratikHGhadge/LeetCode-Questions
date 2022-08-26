5f9dbd1412afec74cb6eb592a5a495546f3d4b9d
Pratik <93422248+PratikHGhadge@users.noreply.github.com>
2022-08-26 21:03:59 +0530

    Time: 0 ms (100.00%), Space: 41.5 MB (12.89%) - LeetHub

--- 461-hamming-distance/461-hamming-distance.java
+++ 461-hamming-distance/461-hamming-distance.java
@@ -1,5 +1,11 @@
 class Solution {
     public int hammingDistance(int x, int y) {
-        return Integer.bitCount(x^y);
+        int t = (x^y);
+        int c = 0;
+        while(t!=0){
+            c = c+(t&1);
+            t = t>>1;
+        }
+        return c;
     }
 }
