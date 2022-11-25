c95af8bcec49f5da5bae3a9561316892dc1877c1
Pratik <93422248+PratikHGhadge@users.noreply.github.com>
2022-11-25 10:43:01 +0530

    Time: 80 ms (98.46%), Space: 40.4 MB (91.54%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -5,37 +5,37 @@ class Solution {
         Boolean[][] visited = new Boolean[r][c];
         for (int i = 0; i < r; i++) {
             for (int j = 0; j < c; j++) {
-                visited[i][j] = false;            }
+                visited[i][j] = false;            
+            }
         }
         for (int i = 0; i < r; i++) {
             for (int j = 0; j < c; j++) {
-                if (board[i][j] == word.charAt(0) && search(board, i, j, word, 0, visited)){
+                if (board[i][j] == word.charAt(0) && search(board, i, j, word, 0)){
                     return true;
                 }
             }
         }
-
         return false;
     }
 
-    private static boolean search(char[][] board, int i, int j, String word, int idx, Boolean[][] visited) {
+    private static boolean search(char[][] board, int i, int j, String word, int idx) {
         if (idx == word.length()-1){
             return true;
         }
-        visited[i][j] = true;
-        if (i>0 && !visited[i-1][j] && board[i-1][j]==word.charAt(idx+1) && search(board, i-1, j, word, idx+1, visited )){
+        board[i][j]-=65;
+        if (i>0 && board[i-1][j]==word.charAt(idx+1) && search(board, i-1, j, word, idx+1 )){
             return true;
         }
-        if (j>0 && !visited[i][j-1] && board[i][j-1]==word.charAt(idx+1) && search(board, i, j-1, word, idx+1, visited )){
+        if (j>0 && board[i][j-1]==word.charAt(idx+1) && search(board, i, j-1, word, idx+1)){
             return true;
         }
-        if (i<board.length-1 && !visited[i+1][j] && board[i+1][j]==word.charAt(idx+1) && search(board, i+1, j, word, idx+1, visited )){
+        if (i<board.length-1 && board[i+1][j]==word.charAt(idx+1) && search(board, i+1, j, word, idx+1)){
             return true;
         }
-        if (j<board[0].length-1 && !visited[i][j+1] && board[i][j+1]==word.charAt(idx+1) && search(board, i, j+1, word, idx+1, visited )){
+        if (j<board[0].length-1 && board[i][j+1]==word.charAt(idx+1) && search(board, i, j+1, word, idx+1 )){
             return true;
         }
-        visited[i][j] = false;
+        board[i][j]+=65;
         return false;
     }
 }
