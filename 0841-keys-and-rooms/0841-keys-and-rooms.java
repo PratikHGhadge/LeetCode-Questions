be1cc0754d33cc3a16e3baa1c40fee1a8adef8e6
Pratik <93422248+PratikHGhadge@users.noreply.github.com>
2022-12-20 20:55:46 +0530

    Time: 1 ms (94.27%), Space: 44.9 MB (33.73%) - LeetHub

---
+++ 0841-keys-and-rooms/0841-keys-and-rooms.java
@@ -0,0 +1,24 @@
+class Solution {
+    boolean[] visited;
+    public boolean canVisitAllRooms(List<List<Integer>> rooms) {
+        visited = new boolean[rooms.size()];
+        visited[0] = true;
+        dfs(rooms, 0);
+        for(boolean r : visited){
+            if(!r){
+                return false;
+            }
+        }
+        return true;
+        
+    }
+    
+    public void dfs(List<List<Integer>> rooms, int index){
+        for(int i : rooms.get(index)){
+            if(!visited[i]){
+                visited[i] = true;
+                dfs(rooms, i);
+            }
+        }
+    }
+}
