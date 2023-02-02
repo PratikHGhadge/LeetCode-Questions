7fb8af2cfcff3c905eb035cb73a3f91f6252a96e
Pratik <93422248+PratikHGhadge@users.noreply.github.com>
2023-02-02 23:18:06 +0530

    Time: 1 ms (85.46%), Space: 41.8 MB (84.09%) - LeetHub

--- 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.java
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.java
@@ -1,6 +1,4 @@
 class Solution {
-    
-    
     public String gcdOfStrings(String str1, String str2) {
         if(!(str1+str2).equals(str2+str1)){
             return "";
@@ -8,12 +6,8 @@ class Solution {
         int validIndex = gcd(str1.length(), str2.length());
         return str1.substring(0, validIndex);
     }
-    
-    
     public int gcd(int a, int b){
         if(b==0) return a;
         return gcd(b, a%b);
     }
-    
-    
 }
