347d6052cc5efea9f1d078e31df2851da85c363d
Pratik <93422248+PratikHGhadge@users.noreply.github.com>
2023-02-12 20:16:30 +0530

    Time: 6 ms (24.98%), Space: 42.2 MB (20.27%) - LeetHub

---
+++ 0791-custom-sort-string/0791-custom-sort-string.java
@@ -0,0 +1,21 @@
+class Solution {
+    public String customSortString(String order, String s) {
+        int[] freq = new int[26];
+        for(int i = 0; i<s.length(); i++){
+            freq[s.charAt(i)-'a']++;
+        }
+        String ans = new String();
+        for(int i = 0; i<order.length(); i++){
+            for(int j = 0; j < freq[order.charAt(i)-'a']; j++){
+                ans+=order.charAt(i);
+            }
+            freq[order.charAt(i)-'a'] = 0;
+        }
+        for(int i = 0; i<26; i++){
+            for(int j = 0; j < freq[i]; j++){
+                ans+=(char)(i+'a');
+            }
+        }
+        return ans;
+    }
+}
