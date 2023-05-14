ae5ae7a3f62e1228a11822d0078bea347baa5e43
Pratik <93422248+PratikHGhadge@users.noreply.github.com>
2023-05-14 20:59:37 +0530

    Time: 10 ms (36.10%), Space: 48 MB (9.72%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -8,19 +8,11 @@ class Solution {
         });
         ArrayList<int[]> ans = new ArrayList<>();
         for(int i = 0; i<intervals.length; i++){
-            int start = intervals[i][0];
-            int end = intervals[i][1];
-            if(!ans.isEmpty() && end <= ans.get(ans.size()-1)[1]) {
-                continue;
+            if(ans.isEmpty() || intervals[i][0] > ans.get(ans.size()-1)[1]){
+                ans.add(intervals[i]);
+            }else{
+                ans.get(ans.size()-1)[1] = Math.max(ans.get(ans.size()-1)[1], intervals[i][1]);
             }
-            for(int j = i+1; j<intervals.length; j++){
-                if(intervals[j][0]<=end){
-                    end = Math.max(end, intervals[j][1]);
-                }else{
-                    break;
-                }
-            }
-            ans.add(new int[]{start, end});
         }
         int[][] newAns = new int[ans.size()][2];
         for(int i = 0; i<ans.size(); i++){
