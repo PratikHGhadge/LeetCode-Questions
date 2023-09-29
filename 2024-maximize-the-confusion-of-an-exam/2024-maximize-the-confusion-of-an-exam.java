05f363ee04a46b70983c94cfd28c0eae913f1bae
Pratik <93422248+PratikHGhadge@users.noreply.github.com>
2023-09-29 22:39:11 +0530

    Time: 17 ms (38.67%), Space: 43.2 MB (96.45%) - LeetHub

--- 2024-maximize-the-confusion-of-an-exam/2024-maximize-the-confusion-of-an-exam.java
+++ 2024-maximize-the-confusion-of-an-exam/2024-maximize-the-confusion-of-an-exam.java
@@ -1,27 +1,17 @@
 class Solution {
     public int maxConsecutiveAnswers(String answerKey, int k) {
+        return Math.max(maxConsecutive(answerKey, k, 'T') , maxConsecutive(answerKey, k, 'F'));
+    }
+    
+    public int maxConsecutive(String s, int k, char ch){
         int ans = 0, l = 0; 
         int zeroCount = 0;
-        for(int r = 0; r<answerKey.length(); r++){
-            if(answerKey.charAt(r)=='T'){
+        for(int r = 0; r<s.length(); r++){
+            if(s.charAt(r)==ch){
                 zeroCount++;
             }
             while(zeroCount>k){
-                if(answerKey.charAt(l)=='T'){
-                    zeroCount--;
-                }
-                l++;
-            }
-            ans = Math.max(ans, r-l+1);
-        }
-        zeroCount = 0;
-        l=0;
-        for(int r = 0; r<answerKey.length(); r++){
-            if(answerKey.charAt(r)=='F'){
-                zeroCount++;
-            }
-            while(zeroCount>k){
-                if(answerKey.charAt(l)=='F'){
+                if(s.charAt(l)==ch){
                     zeroCount--;
                 }
                 l++;
